6009e45a3b4a3aca10886adec0d5fe02c0165c8d
ki3tngu <hunterxd408@gmail.com>
2024-11-04 11:41:56 +0700

    update install script

---
+++ jinro.install.sh
@@ -0,0 +1,13 @@
+pkg install git python python-pip openjdk-17
+pip install mysql-connector-python requests gdown 
+git clone https://github.com/ki3tngu/NgocRongOnline
+clear
+gdown https://drive.usercontent.google.com/download?id=1ZueFYv3k_z_cZT9IRjL7Ltm5pGuBlUVK
+unzip jinro.zip
+rm jinro.zip
+cd NgocRongOnline 
+mv *.sh ~/../usr/bin/
+chmod +x ~/../usr/bin/*.sh
+cd
+clear
+jinro.sh
